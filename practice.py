d80d5eb99f2092fb95bc826ee2b3ff0963195a3d
Yusuke <homekawai22goal@yahoo.co.jp>
2018-11-18 17:36:03 +0900

    プログラムを変更せずとも、入力者が自由にスタート値と幅を設定できるようにした。だいたい多数の場面で登場する値があるときは、一番最初の変数として設定すベき。

--- practice.py
+++ practice.py
@@ -1,16 +1,21 @@
 class Counter:
-    def __init__(self, value):
+    def __init__(self, value, num):
         self.value = value
+        self.num = num
 
     def increase(self):
-        self.value += 1
+        self.value += self.num
 
-    def increase3(self):
-        self.value += 3
+    # def increase3(self):
+    #     self.value += 3
+    #
+    # def increase5(self):
+    #     self.value += 5
+
 
 
 if __name__ == "__main__":
-    counter_1 = Counter(0)
+    counter_1 = Counter(0, 1)
     print(counter_1.value)  # 0 が出力される
 
     counter_1.increase()
@@ -19,11 +24,22 @@ if __name__ == "__main__":
     counter_1.increase()
     print(counter_1.value)  # 2 が出力される  <-- さらに1だけ増えてる!
 
-    counter_2 = Counter(15)
+    counter_2 = Counter(15, 3)
     print(counter_2.value)  # 15 が出力される
 
-    counter_2.increase3()
+    counter_2.increase()
     print(counter_2.value)  # 16 が出力される  <-- 1だけ増えてる!
 
-    counter_2.increase3()
+    counter_2.increase()
     print(counter_2.value)  # 17 が出力される  <-- さらに1だけ増えてる!
+
+    counter_3 = Counter(-5, 5)
+    print(counter_3.value)
+
+    counter_3.increase()
+    print(counter_3.value)
+
+    counter_3.increase()
+    print(counter_3.value)
+
+# -5からプラス5していく
